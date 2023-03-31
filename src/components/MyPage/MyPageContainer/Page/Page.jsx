727d7c565c160e068e9ebfebb0126fc834fa550e
Sergey Georgiev <sgrgv_art@mail.ru>
2023-03-31 20:03:26 +0300

    Fix Page

--- src/components/MyPage/MyPageContainer/Page/Page.jsx
+++ src/components/MyPage/MyPageContainer/Page/Page.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import classes from "./Page.module.css";
 
 let Page = (props) => {
   return (
-    <div>
-      {props.a} {props.b}
+    <div className={classes.wrapper}>
+      <div className={classes.avatar}>avatar</div>
+      <div className={classes.contacts}>about me</div>
+      <div className={classes.newPost}>new post</div>
+      <div className={classes.post}>post</div>
     </div>
   );
 };
