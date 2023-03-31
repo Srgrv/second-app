c0473bb81c579c550a09e7a3b666149fd1849fd1
Sergey Georgiev <sgrgv_art@mail.ru>
2023-03-31 20:53:15 +0300

    Fix initialState

--- src/redux/MyPageReducer.js
+++ src/redux/MyPageReducer.js
@@ -1,6 +1,29 @@
 let initialState = {
-  a: "hello",
+  myPost: ["It is me", "hello world"],
   b: "World",
+  data: {
+    aboutMe: "Халоу",
+    contacts: {
+      facebook: "",
+      website: "electronic.by",
+      vk: "",
+      twitter: "https://twitter.com/@sdf",
+      instagram: "www.instagram.com/posledni_prorok/",
+      youtube: null,
+      github: "github.com/zalash333",
+      mainLink: null,
+    },
+    lookingForAJob: true,
+    lookingForAJobDescription: "Ищуууу чесн слово",
+    fullName: "Jak Zigil`man",
+    userId: 9,
+    photos: {
+      small:
+        "https://social-network.samuraijs.com/activecontent/images/users/9/user-small.jpg?v=0",
+      large:
+        "https://social-network.samuraijs.com/activecontent/images/users/9/user.jpg?v=0",
+    },
+  },
 };
 
 let MyPageReducer = (state = initialState, action) => {
