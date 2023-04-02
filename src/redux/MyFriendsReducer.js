043bb225b248be7ea4903b05965b2a633894f70d
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-02 14:46:38 +0300

    Add MyFriendsReducer

---
+++ src/redux/MyFriendsReducer.js
@@ -0,0 +1,37 @@
+let SET_TOTAL_COUNT = "SET_TOTAL_COUNT";
+let SET_PAGE = "SET_PAGE";
+let SET_DATA = "SET_DATA";
+
+let initialState = {
+  data: [],
+  totalCount: 1000,
+  count: 100,
+  page: 5,
+};
+
+let MyFriendsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_TOTAL_COUNT:
+      return { ...state, totalCount: action.totalCount };
+    case SET_PAGE:
+      return { ...state, page: action.page };
+    case SET_DATA:
+      return { ...state, data: action.data };
+    default:
+      return state;
+  }
+};
+
+export let setTotalCount = (totalCount) => {
+  return { type: SET_TOTAL_COUNT, totalCount };
+};
+
+export let setPage = (page) => {
+  return { type: SET_PAGE, page };
+};
+
+export let setData = (data) => {
+  return { type: SET_DATA, data };
+};
+
+export default MyFriendsReducer;
