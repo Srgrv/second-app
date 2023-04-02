47714f3825d4651820216c2835fbdb842f686ff7
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-02 12:25:23 +0300

    Add Navlink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import classes from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
 
 let Navbar = () => {
-  return <div className={classes.navbar}>Navbar</div>;
+  return (
+    <div className={classes.navbar}>
+      <NavLink to="myPage">Моя Страница</NavLink>
+      <NavLink to="myFriend">Мои Друзья</NavLink>
+    </div>
+  );
 };
 
 export default Navbar;
