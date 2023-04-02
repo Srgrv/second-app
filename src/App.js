aecffdcd83e54143930bfe289daefdad9bfc82c1
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-02 12:26:05 +0300

    Add BrowserRouter, Routes, Route

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import "./App.css";
 import MyPage from "./components/Content/MyPage/MyPage";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Navbar />
-      <div className="Content">
-        <MyPage />
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Navbar />
+        <div className="Content">
+          <Routes>
+            <Route path="/myPage" element={<MyPage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
