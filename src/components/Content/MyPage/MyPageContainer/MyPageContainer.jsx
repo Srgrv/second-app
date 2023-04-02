6edd303270a25533d5fc045c0365997761e6070b
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-02 12:00:56 +0300

    add new function and property to connect

--- src/components/Content/MyPage/MyPageContainer/MyPageContainer.jsx
+++ src/components/Content/MyPage/MyPageContainer/MyPageContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Page from "./Page/Page";
 import { connect } from "react-redux";
+import { changeInput, add } from "../../../../redux/MyPageReducer";
 
 class MyPageContainer extends React.Component {
   render() {
@@ -16,7 +17,8 @@ let mapStateToProps = (state) => {
   return {
     data: state.myPage.data,
     myPost: state.myPage.myPost,
+    input: state.myPage.input,
   };
 };
 
-export default connect(mapStateToProps, {})(MyPageContainer);
+export default connect(mapStateToProps, { changeInput, add })(MyPageContainer);
