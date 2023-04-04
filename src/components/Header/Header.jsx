c1cff0a9ab563e0ac4e4ed4c00f6036b306c6f0e
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-04 17:50:01 +0300

    Fix return

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import classes from "./Header.module.css";
+import HeaderContainer from "./HeaderContainer/HeaderContainer";
+import { NavLink } from "react-router-dom";
 
 let Header = () => {
-  return <div className={classes.header}>Header</div>;
+  return (
+    <div className={classes.header}>
+      <HeaderContainer />
+    </div>
+  );
 };
 
 export default Header;
