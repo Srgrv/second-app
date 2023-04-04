471dbb66d39517ee8418d9872a74cde5366acf61
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-04 17:49:42 +0300

    Add header

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,10 +1,12 @@
 import { combineReducers, legacy_createStore as createStore } from "redux";
 import MyPageReducer from "./MyPageReducer";
 import MyFriendsReducer from "./MyFriendsReducer";
+import HeaderReducer from "./HeaderReducer";
 
 let reducers = combineReducers({
   myPage: MyPageReducer,
   myFriends: MyFriendsReducer,
+  header: HeaderReducer,
 });
 
 let store = createStore(reducers);
