5a08d36028911ca9fcc1c864331543cc6cf17050
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-04 15:41:34 +0300

    Add withRouter and ComponentDidMount

--- src/components/Content/MyPage/MyPageContainer/MyPageContainer.jsx
+++ src/components/Content/MyPage/MyPageContainer/MyPageContainer.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import Page from "./Page/Page";
 import { connect } from "react-redux";
-import { changeInput, add } from "../../../../redux/MyPageReducer";
+import { changeInput, add, setUser } from "../../../../redux/MyPageReducer";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 class MyPageContainer extends React.Component {
+  componentDidMount() {
+    let userId = this.props.router.params.id;
+
+    console.log(userId);
+    if (!userId) {
+      userId = 9;
+    }
+
+    axios
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
+      .then((response) => {
+        this.props.setUser(response.data);
+      });
+  }
+
   render() {
     return (
       <>
@@ -15,10 +32,22 @@ class MyPageContainer extends React.Component {
 
 let mapStateToProps = (state) => {
   return {
-    data: state.myPage.data,
+    user: state.myPage.user,
     myPost: state.myPage.myPost,
     input: state.myPage.input,
   };
 };
 
-export default connect(mapStateToProps, { changeInput, add })(MyPageContainer);
+let withRouter = (Component) => {
+  let ComponentWithRouterProp = (props) => {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return <Component {...props} router={{ params, location, navigate }} />;
+  };
+  return ComponentWithRouterProp;
+};
+
+export default connect(mapStateToProps, { changeInput, add, setUser })(
+  withRouter(MyPageContainer)
+);
