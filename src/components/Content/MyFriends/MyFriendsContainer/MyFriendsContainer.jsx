6cf9f8f7440195ca2db99521faadf40c49d0b1c5
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-04 15:42:23 +0300

    No comment componentDidMount

--- src/components/Content/MyFriends/MyFriendsContainer/MyFriendsContainer.jsx
+++ src/components/Content/MyFriends/MyFriendsContainer/MyFriendsContainer.jsx
@@ -2,26 +2,25 @@ import React from "react";
 import Friends from "./Friends/Friends";
 import { connect } from "react-redux";
 import axios from "axios";
+
 import { setTotalCount } from "../../../../redux/MyFriendsReducer";
 import { setPage } from "../../../../redux/MyFriendsReducer";
 import { setData } from "../../../../redux/MyFriendsReducer";
 import classes from "./MyFriendsContainer.module.css";
 
 class MyFriendsContainer extends React.Component {
-  // componentDidMount() {
-  //   axios
-  //     .get(
-  //       `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.count}&page=${this.props.page}`
-  //     )
-  //     .then((response) => {
-  //       this.props.setData(response.data.items);
-  //       this.props.setTotalCount(response.data.totalCount);
-  //       console.log("componentDidMount");
-  //     });
-  // }
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.count}&page=${this.props.page}`
+      )
+      .then((response) => {
+        this.props.setData(response.data.items);
+        this.props.setTotalCount(response.data.totalCount);
+      });
+  }
 
   changePage(page) {
-    console.log("componentDidUpdate");
     this.props.setPage(page);
     axios
       .get(
@@ -33,16 +32,15 @@ class MyFriendsContainer extends React.Component {
   }
 
   render() {
-    console.log("render");
     let spans = [];
     for (let i = 1; i <= 10; i++) {
       spans.push(i);
     }
 
-    let items = spans.map((item, index) => {
+    let items = spans.map((item) => {
       return (
         <span
-          className={this.props.page == item ? classes.span : undefined}
+          className={this.props.page === item ? classes.span : undefined}
           onClick={() => this.changePage(item)}
           key={item}
         >
