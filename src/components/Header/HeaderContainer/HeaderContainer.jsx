3fd690c0a3b76b711758b7b8d0a805bab0dfd251
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-04 17:50:17 +0300

    Create HeaderContainer

---
+++ src/components/Header/HeaderContainer/HeaderContainer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Header_for_login from "./Header_for_login/Header_for_login";
+import axios from "axios";
+import { setUserData } from "../../../redux/HeaderReducer";
+import { connect } from "react-redux";
+
+class HeaderContainer extends React.Component {
+  componentDidMount() {
+    console.log(this.props);
+    axios
+      .get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        let { id, email, login } = response.data.data;
+        if (response.data.resultCode === 0) {
+          this.props.setUserData(id, email, login);
+        }
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <Header_for_login {...this.props} />
+      </>
+    );
+  }
+}
+
+let mapStateToProps = (state) => {
+  return {
+    id: state.header.id,
+    email: state.header.email,
+    login: state.header.login,
+  };
+};
+
+export default connect(mapStateToProps, { setUserData })(HeaderContainer);
