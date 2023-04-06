69d56ec0dbbd6fba1528e6e37bcf89b31dc82c39
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-06 15:26:31 +0300

    Add post and delete

--- src/components/Content/MyFriends/MyFriendsContainer/Friends/Friends.jsx
+++ src/components/Content/MyFriends/MyFriendsContainer/Friends/Friends.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import classes from "./Friends.module.css";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 let Friends = (props) => {
-  console.log(props);
   return (
     <div>
       {props.data.map((item, index) => {
@@ -26,13 +26,48 @@ let Friends = (props) => {
               {item.followed ? (
                 <button
                   onClick={() => {
-                    props.setUnfollowed(item.id);
+                    axios
+                      .delete(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${item.id}`,
+                        {
+                          withCredentials: true,
+                          headers: {
+                            "API-KEY": "6b94c537-c724-4526-abc4-a919bf086fc0",
+                          },
+                        }
+                      )
+                      .then((response) => {
+                        console.log(response);
+                        if (response.data.resultCode === 0) {
+                          console.log("hello");
+                          props.setUnfollowed(item.id);
+                        }
+                      });
                   }}
                 >
                   Unfollow
                 </button>
               ) : (
-                <button onClick={() => props.setFollowed(item.id)}>
+                <button
+                  onClick={() => {
+                    axios
+                      .post(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${item.id}`,
+                        {},
+                        {
+                          withCredentials: true,
+                          headers: {
+                            "API-KEY": "6b94c537-c724-4526-abc4-a919bf086fc0",
+                          },
+                        }
+                      )
+                      .then((response) => {
+                        if (response.data.resultCode === 0) {
+                          props.setFollowed(item.id);
+                        }
+                      });
+                  }}
+                >
                   Follow
                 </button>
               )}
