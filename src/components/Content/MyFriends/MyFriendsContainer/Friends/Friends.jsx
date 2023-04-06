4f9351965f149ee19c51a6a2183cf9262d1094a9
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-06 16:08:57 +0300

    Fix axios.delete and axios.post

--- src/components/Content/MyFriends/MyFriendsContainer/Friends/Friends.jsx
+++ src/components/Content/MyFriends/MyFriendsContainer/Friends/Friends.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./Friends.module.css";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
+import { followAPI } from "../../../../../API/api";
 
 let Friends = (props) => {
   return (
@@ -26,23 +27,11 @@ let Friends = (props) => {
               {item.followed ? (
                 <button
                   onClick={() => {
-                    axios
-                      .delete(
-                        `https://social-network.samuraijs.com/api/1.0/follow/${item.id}`,
-                        {
-                          withCredentials: true,
-                          headers: {
-                            "API-KEY": "6b94c537-c724-4526-abc4-a919bf086fc0",
-                          },
-                        }
-                      )
-                      .then((response) => {
-                        console.log(response);
-                        if (response.data.resultCode === 0) {
-                          console.log("hello");
-                          props.setUnfollowed(item.id);
-                        }
-                      });
+                    followAPI.deleteUnFollow(item.id).then((data) => {
+                      if (data.resultCode === 0) {
+                        props.setUnfollowed(item.id);
+                      }
+                    });
                   }}
                 >
                   Unfollow
@@ -50,22 +39,11 @@ let Friends = (props) => {
               ) : (
                 <button
                   onClick={() => {
-                    axios
-                      .post(
-                        `https://social-network.samuraijs.com/api/1.0/follow/${item.id}`,
-                        {},
-                        {
-                          withCredentials: true,
-                          headers: {
-                            "API-KEY": "6b94c537-c724-4526-abc4-a919bf086fc0",
-                          },
-                        }
-                      )
-                      .then((response) => {
-                        if (response.data.resultCode === 0) {
-                          props.setFollowed(item.id);
-                        }
-                      });
+                    followAPI.postFollow(item.id).then((data) => {
+                      if (data.resultCode === 0) {
+                        props.setFollowed(item.id);
+                      }
+                    });
                   }}
                 >
                   Follow
